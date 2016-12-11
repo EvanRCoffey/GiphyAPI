58b9e5c499db0476081c8334a4317519a06b76a4
molander86 <rogerecoffey@gmail.com>
2016-12-10 19:17:47 -0600

    Taking a break

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,51 @@
+//Array of ten emotions to be used to make buttons
 var topics = ['happy', 'sad', 'angry', 'scared', 'disgusted', 'sleepy', 'hyper', 'calm', 'confident', 'optimistic'];
-
-$( document ).ready(function() {
-    loadButtons();
-});
 
 //When a button is clicked, load ten static gifs with ratings, then add them all to gifsAndRatings
 $(document).on("click", '.dynamicBtn', function() {
     var emotion = $(this).data("emotion");
     console.log(emotion);
-    for (var i = 0; i<10; i++) {
-    	//Load the first gif and its rating
-    	if (i===0) {
-			$("#gifsAndRatings").html("Gif and rating goes here");
-		}
-		//Append the other nine gifs and their ratings
-		else {
-			$("#gifsAndRatings").append("Gif and rating goes here");
-		}
-    }
+	var queryURL = "http:api.giphy.com/v1/gifs/search?q=" + emotion + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+	// Perfoming an AJAX GET request to our queryURL
+	$.ajax({
+	    url: queryURL,
+	    method: "GET"
+	}) 
+
+	.done(function(response) {
+
+		// Logging the response to our request
+		console.log(response);
+
+		// Putting ten gifs and ratings in "gifsAndRatings"
+		for (var i = 0; i<10; i++) {
+			var imageUrl = response.data[i].images.original_still.url;
+			var emotionImage = $("<img class='gif'>");
+
+			emotionImage.attr("src", imageUrl);
+			emotionImage.attr("alt", "emotion image");
+			emotionImage.attr("width", "200px");
+			emotionImage.attr("height", "200px");
+
+			//Overwrite the HTML for the first GIF
+			if (i === 0) {
+				$("#gifsAndRatings").html(emotionImage);
+				$("#gifsAndRatings").append("<-- Rating: " + response.data[i].rating);
+			}
+			//Append for the rest
+			else {
+				$("#gifsAndRatings").append(emotionImage);
+				$("#gifsAndRatings").append("<-- Rating: " + response.data[i].rating);
+			};
+		};
+	});
 });
 
 //When a gif is clicked...
 	//if it's inactive, activate it
 	//if it's active, deactivate it
 
-//DO THIS PART LAST!
 //When a new word is typed into the form...
 	//Append the word to the end of the array
 	//loadButtons();
@@ -41,3 +62,7 @@ function loadButtons() {
 		}
 	}
 }
+
+$( document ).ready(function() {
+    loadButtons();
+});
